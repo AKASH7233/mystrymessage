aa74e7267d2620847cab84eddd7307a80a3af04a
akash7233 <akashyadav15805@gmail.com>
2024-10-28 17:19:36 +0530

    added next auth in the project

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,77 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import UserModel,{ User } from "@/models/User"
+import bcyrpt from "bcryptjs"
+import connectDatabase from "@/dbConnect/dbConnect"
+
+
+export const authOptions : AuthOptions = {
+    providers : [
+        Credentials({
+            name : "Credentials",
+            credentials : {
+                email : { label : "email", type : "text" },
+                password : { label : "Password", type : "password" },
+            },
+            async authorize(credentials) : Promise<any>{
+                if (!credentials) {
+                    throw new Error("Credentials are required");
+                  }
+          
+            
+                await connectDatabase()
+                try {
+                    const user  = await UserModel.findOne({ $or : [ {email : credentials.email}, {password : credentials.password}] })
+
+                    if(!user){
+                        throw new Error("User not found")
+                    }
+
+                    if(!user.isVerified){
+                        throw new Error("User not verified")
+                    }
+
+                    const isPasswordValid = await bcyrpt.compare(credentials.password, user.password)
+
+                    if(isPasswordValid){
+                        return user
+                    }else{
+                        throw new Error("Invalid password")
+                    }
+
+                } catch (error : any) {
+                    throw new Error(error);
+                }
+            }
+        })
+    ],
+    pages : {
+        signIn : "/signin",
+        signOut : "/signout",
+        error : "/error",
+    },
+    session : {
+        strategy : "jwt"
+    },
+    secret : process.env.NEXT_AUTH_SECRET,
+    callbacks: {
+        async jwt({token, user }) {
+            if(user) {
+                token._id  = user._id?.toString();
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username?.toString();
+            }
+            return token
+        },
+        async session({session, token}) {
+            if(token){
+                session.user._id = token._id;
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessages = token.isAcceptingMessages;
+                session.user.username = token.username;
+            }
+            return session
+        }
+    }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export { default } from "next-auth/middleware"
+import { getToken  } from 'next-auth/jwt'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
+
+    if(token && (
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/verify-email') ||
+        url.pathname.startsWith('/forgot-password') ||
+        url.pathname.startsWith('/reset-password')
+    ) ){
+        return NextResponse.redirect(new URL('/dashboard', url.origin))
+    }
+
+    if(!token && (
+        url.pathname.startsWith('/dashboard') ||
+        url.pathname.startsWith('/profile') ||
+        url.pathname.startsWith('/messages') ||
+        url.pathname.startsWith('/settings') ||
+        url.pathname.startsWith('/logout') 
+    ) ){
+        return NextResponse.redirect(new URL('/sign-in', url.origin))
+    }
+
+    return NextResponse.next()
+}
+ 
+
+export const config = {
+  matcher:[ 
+    '/signin',
+    '/singup',
+    '/',
+    '/dashboard/:path*',
+    '/forgot-password',
+    '/reset-password/[...token]',
+    '/verify-email/[...token]'
+ ],
+}
