0eab862d0e5d468563c6fdc8bf334d28a43f1cd0
akash7233 <akashyadav15805@gmail.com>
2024-10-29 22:10:16 +0530

    created route to send message

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,45 @@
+import connectDatabase from "@/dbConnect/dbConnect";
+import UserModel, { Message } from "@/models/User";
+
+
+export async function POST(req : Request){
+    await connectDatabase();
+
+    try {
+        const { username , content } = await req.json();
+
+        const user = await UserModel.findOne({ username });
+
+        if(!user){
+            return Response.json({
+                success : false,
+                message : "User not found"
+            },{status : 404});
+        }
+
+        const message = {content : content, createdAt : new Date()}
+
+        if(!user.isAcceptingMessage){
+            return Response.json({
+                success : false,
+                message : "User is not accepting messages"
+            },{status : 403});
+        }
+
+        user.message.push(message as Message);
+        await user.save();
+
+        return Response.json({
+            success : true,
+            message : "Message sent successfully",
+            data : message
+        },{status : 200});
+
+    } catch (error) {
+        console.log('Failed to send Message !')
+        return Response.json({
+            success : false,
+            message : "An error occurred while sending message"
+        },{status : 500});
+    }
+}
